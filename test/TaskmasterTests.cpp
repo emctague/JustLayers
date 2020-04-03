2aedfc05ee16d9c916e65fa336ea7479b2af6eea
Ethan McTague <ethan@tague.me>
2020-04-03 14:40:12 -0400

    Add taskmaster unit tests.

---
+++ test/TaskmasterTests.cpp
@@ -0,0 +1,42 @@
+// TaskmasterTests - 
+// 
+// Copyright (c) 2020 Ethan McTague
+
+#include <gtest/gtest.h>
+#include <JustLayers/Taskmaster.h>
+
+class CountAndCancelTask : public jl::ITask {
+public:
+    int count = 0;
+    bool updateTask(jl::Taskmaster *taskmaster) override {
+        count++;
+
+        return (count < 10);
+    }
+};
+
+class CountAndStopTask : public jl::ITask {
+public:
+    int count = 0;
+    bool updateTask(jl::Taskmaster *taskmaster) override {
+        count++;
+
+        if (count >= 20) taskmaster->stop();
+
+        return true;
+    }
+};
+
+TEST(Taskmaster, HandlesTasksCorrectly) {
+    CountAndCancelTask cct;
+    CountAndStopTask cst;
+
+    jl::Taskmaster taskmaster;
+    taskmaster.addTask(jl::IndirectTask::make(&cct));
+    taskmaster.addTask(jl::IndirectTask::make(&cst));
+
+    taskmaster.start();
+
+    ASSERT_EQ(cct.count, 10);
+    ASSERT_EQ(cst.count, 20);
+}
